2d7cb9d244b6d6a631c3254c8158123735386eb6
Cedrick James Orozo <61682645+cedrickjames@users.noreply.github.com>
2025-05-19 07:10:14 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "canteen";
+$dbhost = "192.168.5.6";
+$dbuser = "CanteenSystem";
+$dbpass = "dyBkxSm&jJZ4";
+$dbname = "canteen_transactions";
 
 if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
     die("Failed to Connect to Database!");
